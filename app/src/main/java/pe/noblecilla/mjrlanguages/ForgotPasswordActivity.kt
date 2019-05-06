fb04b22fe76919ed69fbc5a0073a3221cc8afe5f
Luis Noblecilla <noble96luis20@gmail.com>
2019-05-06 13:46:47 -0500

    Forgot Password listo

--- app/src/main/java/pe/noblecilla/mjrlanguages/ForgotPasswordActivity.kt
+++ app/src/main/java/pe/noblecilla/mjrlanguages/ForgotPasswordActivity.kt
@@ -1,12 +1,38 @@
 package pe.noblecilla.mjrlanguages
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_forgot_password.*
 
 class ForgotPasswordActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
+
+        ivBack.setOnClickListener {
+            startActivity(Intent(this, LogInActivity::class.java))
+            finish()
+        }
+
+        btnSend.setOnClickListener {
+            val alert = AlertDialog.Builder(this@ForgotPasswordActivity)
+            alert.setTitle(R.string.dialog_recover_title)
+            alert.setMessage(R.string.dialog_recover_message)
+            alert.setPositiveButton(R.string.dialog_recover_exit){
+                dialog, which ->
+                startActivity(Intent(this, LogInActivity::class.java))
+                finish()
+            }
+
+            val dialog: AlertDialog = alert.create()
+            dialog.show()
+        }
+    }
+
+    override fun onBackPressed() {
+
     }
 }
